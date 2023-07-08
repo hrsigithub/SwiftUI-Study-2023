7eaa48681bedf01574d077ed5291f6160df1c333
hiroro <hrsi.ad@gmail.com>
2023-07-08 10:40:43 +0900

    Initial Commit

---
+++ SwiftUI-Study-2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Study-2023
+//
+//  Created by Hiroshi.Nakai on 2023/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Study-2023/SwiftUI_Study_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Study_2023App.swift
+//  SwiftUI-Study-2023
+//
+//  Created by Hiroshi.Nakai on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Study_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
