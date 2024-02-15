9398fb9203f9ab084593babce10c6f8c04d471de
hrsi <hrsi.ad@gmail.com>
2024-02-15 17:15:36 +0900

    map

---
+++ SwiftUI-Study-2023/Landmarks/Views/Map2View.swift
@@ -0,0 +1,18 @@
+//
+//  Map2View.swift
+//  SwiftUI-Study-2023
+//
+//  Created by Hiroshi.Nakai on 2024/02/15.
+//
+
+import SwiftUI
+
+struct Map2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Map2View()
+}
