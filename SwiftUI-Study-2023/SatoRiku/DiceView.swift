e400e2e6be979b184668b53ac536f2ed1eb7d56c
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-02-17 14:48:01 +0900

    DiceView追加

---
+++ SwiftUI-Study-2023/SatoRiku/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  SwiftUI-Study-2023
+//
+//  Created by Hiroshi.Nakai on 2024/02/17.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
