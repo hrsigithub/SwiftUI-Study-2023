4a3e90abc52ab20a680aa10f33f2f926f6d1cbd9
Hiroshi Nakai <hrsi.ad@gmail.com>
2024-02-19 10:56:09 +0900

    CounterView追加

---
+++ SwiftUI-Study-2023/SatoRiku/CounterView.swift
@@ -0,0 +1,18 @@
+//
+//  CounterView.swift
+//  SwiftUI-Study-2023
+//
+//  Created by Hiroshi.Nakai on 2024/02/19.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CounterView()
+}
